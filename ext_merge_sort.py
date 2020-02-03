b35fdec867f216483a3e6ba33864dde1fcf613a7
pichao <pc@pichaos-MacBook-Pro.local>
2020-02-02 23:29:18 -0800

    merge test not passed

--- ext_merge_sort.py
+++ ext_merge_sort.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+from collections import deque
+import math
 import os
 import sys
 import unittest
@@ -17,7 +19,7 @@ class inbuff:
     def __init__(self, infile):
         super().__init__()
         self.file = infile
-        self.buff = []
+        self.buff = deque()
         self.loc = 0
 
     def read(self, size):
@@ -31,7 +33,17 @@ class inbuff:
                 if i >= start and i < end and v:
                     self.buff.append(int(v))
             logging.info("Fetching result is %s" % str(self.buff))
-        return self.buff
+        return list(self.buff)
+
+    def peek(self):
+        if not self.buff:
+            return None
+        return self.buff[0]
+
+    def pop(self):
+        if not self.buff:
+            return None
+        return self.buff.popleft()
 
 
 class outbuff:
@@ -44,31 +56,96 @@ class outbuff:
         logging.info("Now pushing %d" % v)
         self.buff.append(v)
         if len(self.buff) >= self.size:
-            with open('output/sorted.txt', 'a') as f:
-                for each in self.buff:
-                    f.write(str(each)+'\n')
-            self.buff.clear()
+            self.out()
         logging.info("Buff after push is %s" % str(self.buff))
+
+    def out(self):
+        with open('output/sorted.txt', 'a') as f:
+            for each in self.buff:
+                f.write(str(each)+'\n')
+        self.buff.clear()
 # unit test class
 
 
-class testMerge(unittest.TestCase):
-    def test_init(self):
-        pass
+class mergeSort():
+    def __init__(self):
+        super().__init__()
+        self.inbuffs = []
+        self.outbuff = outbuff(10)
 
-    def test_inbuff(self):
+    def sort(self, files):
+        for file in files:
+            name = file.split('_')[-1]
+            logging.info("Current name is %s" % name)
+            st = []
+            with open('input/'+file, 'r') as f:
+                for line in f:
+                    st.append(int(line))
+            st.sort()
+            with open('tmp/tmp_'+name, 'w') as f:
+                for each in st:
+                    f.write(str(each)+'\n')
+
+    def merge(self, files):
+        for each in files:
+            self.inbuffs.append(inbuff('tmp/'+each))
+            self.inbuffs[-1].read(9)
+            logging.info("The initial inbuff of %s is %s" %
+                         (each, str(self.inbuffs[-1].buff)))
+        while True:
+            m = math.inf
+            ind = -1
+            empty = 0
+            logging.info("Now start to finding minimum")
+            for i, v in enumerate(self.inbuffs):
+                empty = 0
+                if v.peek() == None:
+                    empty += 1
+                    continue
+                elif v.peek() < m:
+                    ind = i
+                    m = v.peek()
+            if empty == 10:
+                self.outbuff.out()
+                break
+
+            logging.info("Found %d from %dth buff is smaller then %d" %
+                         (self.inbuffs[ind].peek(), ind, m))
+            m = self.inbuffs[ind].pop()
+            if not self.inbuffs[ind].peek:
+                logging.info("Now the %dth buff is empty" % ind)
+                self.inbuffs[ind].read(9)
+            self.outbuff.push(m)
+
+
+class testMerge(unittest.TestCase):
+    def _test_init(self):
+        files = os.listdir('input')
+
+    def _test_inbuff(self):
         foo = inbuff("intest.txt")
         self.assertEqual([i for i in range(10)], foo.read(10))
+        self.assertEqual(0, foo.peek())
+        self.assertEqual(0, foo.pop())
         self.assertEqual([i for i in range(10, 20)], foo.read(10))
         self.assertEqual([], foo.read(1))
 
-    def test_outbuff(self):
+    def _test_outbuff(self):
         foo = outbuff(10)
         for i in range(9):
             foo.push(i)
             self.assertEqual([j for j in range(i+1)], foo.buff)
         foo.push(9)
         self.assertEqual([], foo.buff)
+
+    def _test_sort(self):
+        foo = mergeSort()
+        foo.sort(os.listdir('input'))
+
+    def test_merge(self):
+        foo = mergeSort()
+        files = os.listdir('tmp')
+        foo.merge(files)
 
 
 if __name__ == "__main__":
